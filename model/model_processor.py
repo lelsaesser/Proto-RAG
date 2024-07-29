558b3912a07f70ae56d2aa99a80ec35814b78698
D071137 <lucas.elsaesser@gmail.com>
2024-07-29 10:33:54 +0200

    first try with embeddings using stella model

---
+++ model/model_processor.py
@@ -0,0 +1,12 @@
+from sentence_transformers import SentenceTransformer
+
+docs = [
+    "There are many effective ways to reduce stress. Some common techniques include deep breathing, meditation, and physical activity. Engaging in hobbies, spending time in nature, and connecting with loved ones can also help alleviate stress. Additionally, setting boundaries, practicing self-care, and learning to say no can prevent stress from building up.",
+    "Green tea has been consumed for centuries and is known for its potential health benefits. It contains antioxidants that may help protect the body against damage caused by free radicals. Regular consumption of green tea has been associated with improved heart health, enhanced cognitive function, and a reduced risk of certain types of cancer. The polyphenols in green tea may also have anti-inflammatory and weight loss properties.",
+]
+
+# ！The default dimension is 1024, if you need other dimensions, please clone the model and modify `modules.json` to replace `2_Dense_1024` with another dimension, e.g. `2_Dense_256` or `2_Dense_8192` !
+model = SentenceTransformer("dunzhang/stella_en_400M_v5", trust_remote_code=True)
+doc_embeddings = model.encode(docs)
+print(doc_embeddings.shape)
+print(doc_embeddings)
